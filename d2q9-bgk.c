87d153aca4135fa7a52bbbe8deb3674ac2b068d1
matthew-nagy <matttnagy@gmail.com>
2022-02-01 16:22:31 +0000

    rebound has been optimised

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -269,30 +269,34 @@ int propagate(const t_param params, t_speed* cells, t_speed* tmp_cells)
 
 int rebound(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles)
 {
-  /* loop over the cells in the grid */
-  for (int jj = 0; jj < params.ny; jj++)
-  {
-    for (int ii = 0; ii < params.nx; ii++)
-    {
-      /* if the cell contains an obstacle */
-      if (obstacles[jj*params.nx + ii])
-      {
-        /* called after propagate, so taking values from scratch space
-        ** mirroring, and writing into main grid */
-        // cells[ii + jj*params.nx].speeds[1] = tmp_cells[ii + jj*params.nx].speeds[3];
-        // cells[ii + jj*params.nx].speeds[2] = tmp_cells[ii + jj*params.nx].speeds[4];
-        // cells[ii + jj*params.nx].speeds[3] = tmp_cells[ii + jj*params.nx].speeds[1];
-        // cells[ii + jj*params.nx].speeds[4] = tmp_cells[ii + jj*params.nx].speeds[2];
-        // cells[ii + jj*params.nx].speeds[5] = tmp_cells[ii + jj*params.nx].speeds[7];
-        // cells[ii + jj*params.nx].speeds[6] = tmp_cells[ii + jj*params.nx].speeds[8];
-        // cells[ii + jj*params.nx].speeds[7] = tmp_cells[ii + jj*params.nx].speeds[5];
-        // cells[ii + jj*params.nx].speeds[8] = tmp_cells[ii + jj*params.nx].speeds[6];
-        float* temp = cells[ii + jj*params.nx].speeds;
-        cells[ii + jj*params.nx].speeds = tmp_cells[ii + jj*params.nx].speeds;
-        tmp_cells[ii + jj*params.nx].speeds = temp;
-      }
-    }
-  }
+  t_speed* temp = cells;
+  cells = tmp_cells;
+  tmp_cells = temp;
+  // /* loop over the cells in the grid */
+  // for (int jj = 0; jj < params.ny; jj++)
+  // {
+  //   for (int ii = 0; ii < params.nx; ii++)
+  //   {
+  //     /* if the cell contains an obstacle */
+  //     if (obstacles[jj*params.nx + ii])
+  //     {
+  //       /* called after propagate, so taking values from scratch space
+  //       ** mirroring, and writing into main grid */
+  //       // cells[ii + jj*params.nx].speeds[1] = tmp_cells[ii + jj*params.nx].speeds[3];
+  //       // cells[ii + jj*params.nx].speeds[2] = tmp_cells[ii + jj*params.nx].speeds[4];
+  //       // cells[ii + jj*params.nx].speeds[3] = tmp_cells[ii + jj*params.nx].speeds[1];
+  //       // cells[ii + jj*params.nx].speeds[4] = tmp_cells[ii + jj*params.nx].speeds[2];
+  //       // cells[ii + jj*params.nx].speeds[5] = tmp_cells[ii + jj*params.nx].speeds[7];
+  //       // cells[ii + jj*params.nx].speeds[6] = tmp_cells[ii + jj*params.nx].speeds[8];
+  //       // cells[ii + jj*params.nx].speeds[7] = tmp_cells[ii + jj*params.nx].speeds[5];
+  //       // cells[ii + jj*params.nx].speeds[8] = tmp_cells[ii + jj*params.nx].speeds[6];
+  //       float* temp = cells[ii + jj*params.nx].speeds;
+  //       cells[ii + jj*params.nx].speeds = tmp_cells[ii + jj*params.nx].speeds;
+  //       tmp_cells[ii + jj*params.nx].speeds = temp;
+  //     }
+  //   }
+  // }
+
 
   return EXIT_SUCCESS;
 }
