056433f047536e4b0b0389c2017488f97695cb40
matthew-nagy <matttnagy@gmail.com>
2022-02-07 14:05:16 +0000

    Reverting time

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -250,7 +250,7 @@ int accelerate_flow(const t_param params, t_speed* cells, int* obstacles)
   return EXIT_SUCCESS;
 }
 
-inline void cellProp(int y_n, int x_e, int y_s, int x_w, int ii, int jj, const t_param params, t_speed* cells, t_speed* tmp_cells){
+inline void propagateSwap(const t_param params, t_speed* cells, t_speed* tmp_cells, int ii, int jj, int y_n, int x_w, int y_s, int x_w){
   /* propagate densities from neighbouring cells, following
   ** appropriate directions of travel and writing into
   ** scratch space grid */
@@ -268,41 +268,19 @@ inline void cellProp(int y_n, int x_e, int y_s, int x_w, int ii, int jj, const t
 int propagate(const t_param params, t_speed* cells, t_speed* tmp_cells)
 {
   /* loop over _all_ cells */
-  const int jjLimit = params.ny - 1;
-  const int iiLimit = params.nx - 1;
-  for (int jj = 0; jj < jjLimit; jj++)
+  for (int jj = 0; jj < params.ny; jj++)
   {
-    int y_n = jj + 1;
-    int y_s = (jj == 0) ? (jj + params.ny - 1) : (jj - 1);
-    for (int ii = 0; ii < iiLimit; ii++)
-    //TO DO, try manually creating neighbouring versions of the loop, so y_n, x_e doesn't need mod
-    //Maybe a ternary branch?
+    for (int ii = 0; ii < params.nx; ii++)
     {
       /* determine indices of axis-direction neighbours
       ** respecting periodic boundary conditions (wrap around) */
-      //int x_e = (ii + 1) % params.nx;
-      int x_e = ii + 1;
+      int y_n = (jj + 1) % params.ny;
+      int x_e = (ii + 1) % params.nx;
+      int y_s = (jj == 0) ? (jj + params.ny - 1) : (jj - 1);
       int x_w = (ii == 0) ? (ii + params.nx - 1) : (ii - 1);
 
-      cellProp(y_n, x_e, y_s, x_w, ii, jj, params, cells, tmp_cells);
+      propagateSwap(params, cells, tmp_cells, ii, jj, y_n, x_w, y_s, x_w);
     }
-    int x_e = 0;
-    int x_w = params.nx - 2;
-    cellProp(y_n, x_e, y_s, x_w, params.nx-1, jj, params, cells, tmp_cells);
-  }
-  int y_n = 0;
-  int y_s = jjLimit - 1;
-  for (int ii = 0; ii < iiLimit; ii++)
-  //TO DO, try manually creating neighbouring versions of the loop, so y_n, x_e doesn't need mod
-  //Maybe a ternary branch?
-  {
-    /* determine indices of axis-direction neighbours
-    ** respecting periodic boundary conditions (wrap around) */
-    //int x_e = (ii + 1) % params.nx;
-    int x_e = ii + 1;
-    int x_w = (ii == 0) ? (ii + params.nx - 1) : (ii - 1);
-
-    cellProp(y_n, x_e, y_s, x_w, ii, jjLimit, params, cells, tmp_cells);
   }
 
   return EXIT_SUCCESS;
